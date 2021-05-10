fc2ef6fa7173fee7834c9267ff8a4518a6f0f8c2
JohnAVelaP <83561586+JohnAVelaP@users.noreply.github.com>
2021-05-09 22:16:51 -0500

    Add files via upload

---
+++ Usuarios.API/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Usuarios.BI.Data;
+using Usuarios.Entities.Entities;
+
+namespace Usuarios.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly Context contextapp;
+        public UsuarioController(Context context)
+        {
+            this.contextapp = context;
+        }
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> Get()
+        {
+            return contextapp.Usuario.ToList();
+        }
+    }
+}
---
+++ Usuarios.Data/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Usuarios.Entities.Entities;
+
+namespace Usuarios.BI.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
