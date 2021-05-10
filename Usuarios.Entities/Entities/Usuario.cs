0524378ce5cae116b7196211e70be26de6f3d425
JohnAVelaP <83561586+JohnAVelaP@users.noreply.github.com>
2021-05-09 22:17:39 -0500

    Add files via upload

---
+++ Usuarios.Entities/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Usuarios.Entities.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public decimal UsuID { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
